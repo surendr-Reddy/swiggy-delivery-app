93ff0cf77bd79ad2dc763c5aaa07eea88aa731e5
surendr-Reddy <surendr.8reddy@gmail.com>
2024-02-19 23:27:34 +0530

    async await use cases

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,31 +1,50 @@
 import RestraurantCard from "./RestraurantCard";
 import { restaurantCardsData } from "./utils/mockData";
-import { useState}  from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
+  const [rescared, setRescard] = useState([]);
 
-    const [rescared, setRescard]=useState(restaurantCardsData);
-    const filterdata = ()=>{
-        const filterdata = rescared.filter((data)=>{
-           return data.info.avgRating   >4
-            
-        })
-        setRescard(filterdata);
-    }
-    return (
-      <div className="restraurant-contaniner">
-        <div className="search-bar">
-          <h3>Serach-bar</h3>
-        </div>
-        <div>
-            <button className="avg-filter" onClick={filterdata}>topRest</button>
-        </div>
-        <div className="restraurant-card">
-          {rescared.map((resCrad) => {
-            return <RestraurantCard key={resCrad.info.id} resData={resCrad} />;
-          })}
-        </div>
-      </div>
+  useEffect(  async() => {
+    //agaian waiting for the promise resolve 
+    const resData =   await resCardsData();
+    console.log(resData);
+  }, []);
+
+  const resCardsData = async () => {
+    
+    const apiData= await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
+    // waitng for the json
+    const apisuData=await apiData.json();
+    return  apisuData// return promise 
+    
+
   };
-  export default Body;
+
+  const filterdata = () => {
+    const filterdata = rescared.filter((data) => {
+      return data.info.avgRating > 4;
+    });
+    setRescard(filterdata);
+  };
+  return (
+    <div className="restraurant-contaniner">
+      <div className="search-bar">
+        <h3>Serach-bar</h3>
+      </div>
+      <div>
+        <button className="avg-filter" onClick={filterdata}>
+          topRest
+        </button>
+      </div>
+      <div className="restraurant-card">
+        {rescared.map((resCrad) => {
+          return <RestraurantCard key={resCrad.info.id} resData={resCrad} />;
+        })}
+      </div>
+    </div>
+  );
+};
+export default Body;
